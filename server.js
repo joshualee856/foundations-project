b9f673f9f37026dec2c4c9c3b2fdabc216a3a484
joshualee856 <joshua856@revature.net>
2024-02-19 15:10:13 -0800

    Set up server file

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!')
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+})

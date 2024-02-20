6e89610b5bed47311db3db6851c57faca84ccfe3
joshualee856 <joshua856@revature.net>
2024-02-20 14:00:32 -0800

    Code register and login endpoints

--- server.js
+++ server.js
@@ -2,7 +2,38 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+    let body = '';
 
+    req
+        .on('data', (chunk) => {
+            body += chunk;
+        })
+        .on('end', () => {
+            body = body.length > 0 ? JSON.parse(body) : {};
+
+            const contentType = {"ContentType": "application/json"};
+
+            if (req.url.startsWith('/register')) {
+                if (req.method === 'POST') {
+                    res.writeHead(200, contentType);
+                    res.end(JSON.stringify({message: "This is the /register endpoint", body }));
+                } else {
+                    res.writeHead(404, contentType);
+                    res.end(JSON.stringify({error: "Invalid HTTP Method"}))
+                }
+            } else if (req.url.startsWith('/login')) {
+                if (req.method === 'POST') {
+                    res.writeHead(200, contentType);
+                    res.end(JSON.stringify({message: "This is the /login endpoint", body }));
+                } else {
+                    res.writeHead(404, contentType);
+                    res.end(JSON.stringify({error: "Invalid HTTP Method"}));
+                }
+            } else {
+                res.writeHead(404, contentType);
+                res.end(JSON.stringify({error: "Invalid Endpoint"}));
+            }
+        })
 })
 
 server.listen(PORT, () => {

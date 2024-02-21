2d8e479fa75fd4f971d991e74212d121f5a88375
joshualee856 <joshua856@revature.net>
2024-02-21 14:31:06 -0800

    Create /login endpoint handler

--- src/controller/LoginRouter.js
+++ src/controller/LoginRouter.js
@@ -3,8 +3,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json({ message: 'This is the /login endpoint!'})
+const employeeService = require('../service/EmployeeService');
+
+router.post('/', async (req, res) => {
+    // console.log(req.body);
+    const data = await employeeService.postLogin(req.body);
+    if (!data.error) {
+        res.status(202).json(`${data.role}`)
+    } else {
+        res.status(400).json(data.error);
+    }
 })
+
+// router.get('/', (req, res) => {
+//     res.status(200).json({ message: 'This is the /login endpoint!'})
+// })
 
 module.exports = router;

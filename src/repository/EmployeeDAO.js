3755a9aad34ed3f981895a945a506fd384293d5b
joshualee856 <joshua856@revature.net>
2024-02-21 12:27:27 -0800

    Create postEmployee DAO method to add employees to database

--- src/repository/EmployeeDAO.js
+++ src/repository/EmployeeDAO.js
@@ -1,1 +1,32 @@
-console.log('This is the Employee DAO file');
+const { DynamoDBClient, QueryCommand } = require('@aws-sdk/client-dynamodb');
+const {
+    DynamoDBDocumentClient, 
+    GetCommand,
+    PutCommand,
+} = require('@aws-sdk/lib-dynamodb');
+
+const logger = require('../util/Logger');
+
+const client = new DynamoDBClient({ region: 'us-west-1' });
+
+const documentClient = DynamoDBDocumentClient.from(client);
+
+const TableName = 'Employees';
+
+async function postEmployee(Item) {
+    const command = new PutCommand({
+        TableName,
+        Item
+    });
+
+    try {
+        const data = await documentClient.send(command);
+        return data;
+    } catch(error) {
+        logger.error(error);
+    }
+}
+
+module.exports = {
+    postEmployee,
+}

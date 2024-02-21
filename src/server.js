d8e6fbfe81139e6fd1ef1d52b5d67c5813b68ea8
joshualee856 <joshua856@revature.net>
2024-02-21 10:09:01 -0800

    Refactor server file to use Express

--- src/server.js
+++ src/server.js
@@ -1,39 +1,12 @@
-const http = require('http');
+const express = require('express');
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    let body = '';
+const server = express();
 
-    req
-        .on('data', (chunk) => {
-            body += chunk;
-        })
-        .on('end', () => {
-            body = body.length > 0 ? JSON.parse(body) : {};
+server.use(express.json());
 
-            const contentType = {"ContentType": "application/json"};
-
-            if (req.url.startsWith('/register')) {
-                if (req.method === 'POST') {
-                    res.writeHead(200, contentType);
-                    res.end(JSON.stringify({message: "This is the /register endpoint", body }));
-                } else {
-                    res.writeHead(404, contentType);
-                    res.end(JSON.stringify({error: "Invalid HTTP Method"}))
-                }
-            } else if (req.url.startsWith('/login')) {
-                if (req.method === 'POST') {
-                    res.writeHead(200, contentType);
-                    res.end(JSON.stringify({message: "This is the /login endpoint", body }));
-                } else {
-                    res.writeHead(404, contentType);
-                    res.end(JSON.stringify({error: "Invalid HTTP Method"}));
-                }
-            } else {
-                res.writeHead(404, contentType);
-                res.end(JSON.stringify({error: "Invalid Endpoint"}));
-            }
-        })
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello World!'})
 })
 
 server.listen(PORT, () => {

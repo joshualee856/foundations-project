2600b08bfe787bfd968beb861b83c1b0ae0cd390
joshualee856 <joshua856@revature.net>
2024-02-21 12:29:39 -0800

    Create /register endpoint handler

--- src/controller/RegisterRouter.js
+++ src/controller/RegisterRouter.js
@@ -3,8 +3,19 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json({ message: 'This is the /register endpoint!'})
+const employeeService = require('../service/EmployeeService');
+
+// router.get('/', (req, res) => {
+//     res.status(200).json({ message: 'This is the /register endpoint!'})
+// })
+
+router.post('/', async (req, res) => {
+    const data = await employeeService.postRegister(req.body);
+    if (data) {
+        res.status(201).json({ message: 'Registration Successful '})
+    } else {
+        res.status(400).json({ error: 'Username is already taken' })
+    }
 })
 
 module.exports = router;

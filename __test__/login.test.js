b2f5960a3dfa590e46e1f731652887ccdbf68853
joshualee856 <joshua856@revature.net>
2024-02-21 20:22:16 -0800

    Create unit tests for employee login functionality

---
+++ __test__/login.test.js
@@ -0,0 +1,43 @@
+const { postLogin } = require('../src/service/EmployeeService');
+
+describe('Employee Login Tests', () => {
+    test('Logging in with a blank username and/or password should return an error message', async () => {
+        let response;
+        let employee = {
+            username: '',
+            password: ''
+        }
+        let expectedResult = { error: 'The username and password cannot be blank' };
+
+        response = await postLogin(employee);
+        
+        expect(response).toEqual(expectedResult);
+    })
+
+    test('Logging in with a username and password that does not exist in the database should return an error message', async () => {
+        let response;
+        let employee = {
+            username: 'Lucy',
+            password: 'doggo'
+        }
+        let expectedResult = { error: 'Invalid Credentials' };
+
+        response = await postLogin(employee);
+
+        expect(response).toEqual(expectedResult);
+    })
+
+    test('Logging in with a username and password that does exist in the database should return the employee\'s data', async () => {
+        const employeeDAO = require('../src/repository/EmployeeDAO');
+        let response;
+        let employee = {
+            username: 'RevaturePro',
+            password: 'RevPro'
+        }
+        let expectedResult = await employeeDAO.getEmployee(employee);
+
+        response = await postLogin(employee);
+
+        expect(response).toEqual(expectedResult);
+    })
+})

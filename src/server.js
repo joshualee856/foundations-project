4afc2e67c90cd34a35e58276a5d9711e1c399274
joshualee856 <joshua856@revature.net>
2024-02-23 15:22:47 -0800

    Create /tickets endpoint on server file

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 // Endpoint Handlers
 const registerRouter = require('./controller/RegisterRouter');
 const loginRouter = require('./controller/LoginRouter');
+const ticketRouter = require('./controller/TicketRouter')
 
 const server = express();
 
@@ -12,6 +13,7 @@ server.use(express.json());
 // Endpoints
 server.use('/register', registerRouter);
 server.use('/login', loginRouter);
+server.use('/tickets', ticketRouter)
 
 server.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);

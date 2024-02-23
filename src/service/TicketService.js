610053292fd6ccfe14d0e75ec19bf0f5a60db6c3
joshualee856 <joshua856@revature.net>
2024-02-23 15:21:21 -0800

    Code create ticket service method

--- src/service/TicketService.js
+++ src/service/TicketService.js
@@ -1,3 +1,28 @@
 const ticketDAO = require('../repository/TicketDAO');
 const uuid = require('uuid');
 
+async function createTicket(ticket) {
+
+    if (!ticket.description) {
+        return { error: 'Requests cannot be submitted without a description' }
+    } else if (!ticket.amount) {
+        return { error: 'Requests cannot be submitted without an amount' }
+    } else {
+        let newTicket = {
+            id: uuid.v4(),
+            author: ticket.author,
+            description: ticket.description,
+            type: ticket.type,
+            amount: ticket.amount
+        }
+        
+        await ticketDAO.insertTicket(newTicket);
+
+        // Retrieve and return the newly inserted ticket
+        return await ticketDAO.getTicket(newTicket.id);
+    }
+}
+
+module.exports = {
+    createTicket,
+}

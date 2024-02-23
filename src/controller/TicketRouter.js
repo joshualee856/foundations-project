bb930afd598e10cea93a5b0d3b4e2f2e004af194
joshualee856 <joshua856@revature.net>
2024-02-23 15:21:50 -0800

    Create endpoint handler for ticket submission

--- src/controller/TicketRouter.js
+++ src/controller/TicketRouter.js
@@ -4,3 +4,14 @@ const express = require('express');
 const router = express.Router();
 
 const ticketService = require('../service/TicketService');
+
+router.post('/', async (req, res) => {
+    const ticket = await ticketService.createTicket(req.body);
+    if (!ticket.error) {
+        res.status(202).json(ticket)
+    } else {
+        res.status(400).json(ticket.error);
+    }
+})
+
+module.exports = router;
